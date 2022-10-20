b3f8b98d5d11376ecf8c9794d07853d621261427
maxiibrunaa123 <maxiibrunaa123@gmail.com>
2022-10-20 09:46:36 -0300

    New Commit

--- src/com/maxi/BucleMain.java
+++ src/com/maxi/BucleMain.java
@@ -1,2 +1,22 @@
-package com.maxi;public class BucleMain {
+package com.maxi;
+
+/*
+En este ejercicio tenéis que crear un bucle que permita concatenar textos e imprima el resultado final por consola.
+
+Tened en cuenta que los textos pueden venir de un array de tipo String. Por ejemplo:
+
+String[] nombres = {"", "", "", ""}
+
+ */
+
+public class BucleMain {
+    public static void main(String[] args) {
+
+        String[] saludos = {"Hola","buenos","dias"};
+
+        for (String saludo:saludos) {
+            System.out.print(" " + saludo);
+        }
+
+    }
 }
